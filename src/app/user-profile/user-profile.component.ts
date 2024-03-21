cc9d86b473921809acb5beff36cce98510f332ac
Elie Bendier <elie.bendier.dev@gmail.com>
2024-03-21 14:51:24 +0100

    created User interface and User-profile component, implemented it in the template

---
+++ src/app/user-profile/user-profile.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { User } from '../user';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-profile',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-profile.component.html',
+  styleUrl: './user-profile.component.scss'
+})
+export class UserProfileComponent {
+  user: User = {
+    name: 'Doe',
+    firstName: 'John',
+    age: 25,
+    quote: '',
+    photo: 'https://randomuser.me/api/portraits/lego/2.jpg',
+  }
+
+  isShow = true;
+
+  displayAge(): void {
+    this.isShow = !this.isShow;
+  }
+}
