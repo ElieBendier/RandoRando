54e80f6c3c614df3a42895232a96833262700f3d
Elie Bendier <elie.bendier.dev@gmail.com>
2024-03-21 16:57:36 +0100

    Testing attribute directives and structural directives to switch button and color title on button click switch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,20 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BlockComponent } from './block/block.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UserProfileComponent],
+  imports: [RouterOutlet, BlockComponent, UserProfileComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Elie Bendier';
+  isAdmin: boolean = true;
+
+  switchIsAdmin(): void {
+    this.isAdmin = !this.isAdmin;
+  }
 }
